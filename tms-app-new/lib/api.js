f276617d982d9b5097369be2f35a4e5646f1fdb3
daan-vlh <s148821@ap.be>
2025-11-29 14:40:01 +0100

    added necessary folder structure and added basic code

---
+++ tms-app-new/lib/api.js
@@ -0,0 +1,11 @@
+import { API_URL } from "../constants/Api";
+
+export const api = {
+  login: `${API_URL}/auth/login`,
+  register: `${API_URL}/auth/register`,
+
+  users: `${API_URL}/users`,
+  orders: `${API_URL}/orders`,
+  stops: `${API_URL}/stops`,
+  trucks: `${API_URL}/trucks`,
+};
---
+++ tms-app-new/theme/colors.js
@@ -0,0 +1,9 @@
+export default {
+  accent: "#355BE4",
+  text: "#141414ff",
+  background: "#F4F5F6",
+  backgroundOnTop: "#ffffff",
+  backgroundDarker: "#E6E5E5",
+  border: "rgba(0, 0, 0, 0.10)",
+  transparent: "#ffffff",
+};
